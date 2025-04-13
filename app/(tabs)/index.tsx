b324f5de7ec55f4ed1251707991a239ce8c022fc
Anamika Aggarwal <anamikaagg07@gmail.com>
2025-04-13 16:51:16 +0530

    integrate .env

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ export default function HomeScreen() {
 
   const searchStock = async () => {
     try {
-      const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${search}&apikey=9TUQLE0LFZH1GKXC`);
+      const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${search}&apikey=${process.env.EXPO_PUBLIC_API_KEY}`);
       const data = await response.json();
       console.log('data', data);
       setStockData(data);
